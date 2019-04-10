6c3dea1bd4f93a0144ac2be80f5f45ecef4657a9
Grant Valentine <grantsvalentine@gmail.com>
2019-04-10 13:01:32 -0600

    fkfkfkfkfkfkf

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+# from django.urls import path, include
 
-from django.contrib import admin
-from django.conf.urls import url, include
-admin.autodiscover()
+# from django.contrib import admin
+# from django.conf.urls import url, include
+# admin.autodiscover()
 
-import hello
+# import hello
 
 # To add a new path, first import the app:
 # import blog
@@ -14,10 +14,25 @@ import hello
 #
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
+# urlpatterns = [
+#      path("", hello.views.index, name="index"),
+#     #  path("db/", hello.views.db, name="db"),
+#     # path("admin/", admin.site.urls),
+#     url(r'^admin/', admin.site.urls),
+#     # url('', include('django_mako_plus.urls')),
+# ]
+
+# original Django 
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+import hello.views
+
 urlpatterns = [
-     path("", hello.views.index, name="index"),
-     path("db/", hello.views.db, name="db"),
-    # path("admin/", admin.site.urls),
-    url(r'^admin/', admin.site.urls),
-    url('', include('django_mako_plus.urls')),
+    path("", hello.views.index, name="index"),
+    path("db/", hello.views.db, name="db"),
+    path("admin/", admin.site.urls),
 ]
