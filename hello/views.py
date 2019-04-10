658898047ea71c72baad79ca6ec030691faae31a
JD Davenport <jddavenport46@gmail.com>
2019-04-10 05:08:58 -0600

    asfds

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = 'dam'
+    r = 'dam before urls can be ready'
     print(r)
     return HttpResponse('<pre>' + r + '</pre>')
 
