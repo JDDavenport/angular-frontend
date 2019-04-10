bffd95b7ffc5375f7c4f34eea9943d458b86c285
Grant Valentine <grantsvalentine@gmail.com>
2019-04-10 12:12:30 -0600

    fk u

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 admin.autodiscover()
 
-#import hello.views
+import hello
 
 # To add a new path, first import the app:
 # import blog
