d771f165972fba387ad3fd64a00b2927259db2fa
Grant Valentine <grantsvalentine@gmail.com>
2019-04-10 11:32:45 -0600

    Doing stuff

---
+++ hello/views/search.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponseRedirect
+from django_mako_plus import view_function
+from django import forms
+from homepage import models as hmod
+from django.contrib.auth import models as pmod
+
+
+
+@view_function
+def process_request(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            searchTerm = form.cleaned_data['userInput']   
+#            p1 = pmod.Permission.objects.get(id=1)
+            a1 = hmod.overdoses.objects.filter(abbrev = 'UT')
+            searchTerm = a1.abbrev
+            args = {'form': form, 'searchTerm': searchTerm}
+            return request.dmp.render('search.html', args)
+    else:
+        searchTerm = "none"
+        form = MyForm()
+    args = {'form': form, 'searchTerm': searchTerm}
+    return request.dmp.render('search.html', args)
+
+class MyForm(forms.Form):
+    '''An example form'''
+    userInput = forms.CharField(label='Search Term')
+
+    def clean_data(self):
+        return self.cleaned_data 
