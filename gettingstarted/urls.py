86442527ddd9b080e48af167c860ff5e812694a0
JD Davenport <jddavenport46@gmail.com>
2019-04-10 10:23:29 -0600

    urls added

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from django.contrib import admin
-
+from django.conf.urls import url, include
 admin.autodiscover()
 
 import hello.views
@@ -18,4 +18,5 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    url('', include('django_mako_plus.urls')),
 ]
