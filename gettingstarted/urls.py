1ef1a74ea10ab76be820414d3c6720db436a15bc
Grant Valentine <grantsvalentine@gmail.com>
2019-04-10 11:19:47 -0600

    DMP File Structure

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,8 +15,9 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
-    path("admin/", admin.site.urls),
+    # path("", hello.views.index, name="index"),
+    # path("db/", hello.views.db, name="db"),
+    # path("admin/", admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url('', include('django_mako_plus.urls')),
 ]
